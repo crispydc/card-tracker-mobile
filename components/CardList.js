83d90138f6ad125ae493388c6e4564b6c638cf81
Christopher Perry <cperry@sapient.com>
2016-03-11 16:19:37 -0500

    Made card list scrollable.

--- components/CardList.js
+++ components/CardList.js
@@ -1,6 +1,7 @@
 'use strict';
 import React, {
   View,
+  ScrollView,
   Text,
   TouchableNativeFeedback,
   StyleSheet,
@@ -13,9 +14,11 @@ var CardList = React.createClass({
   render() {
     return (
       <View style={styles.cardList}>
-        {this.props.cardList.map((card) => {
-          return <CardListItem key={card.id} card={card} uid={this.props.uid} />;
-        })}
+        <ScrollView>
+          {this.props.cardList.map((card) => {
+            return <CardListItem key={card.id} card={card} uid={this.props.uid} />;
+          })}
+        </ScrollView>
       </View>
     );
   }
